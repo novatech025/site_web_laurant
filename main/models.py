d3345c589a401ea506cb697efbf58e6b2316fdad
KNJSoft <knjprod@gmail.com>
2024-11-16 20:33:29 +0100

    site_web_laurent delete tables

--- main/models.py
+++ main/models.py
@@ -4,35 +4,19 @@ from django.db import models
 # Create your models here.
 
 
-class Sable(models.Model):
-    TYPE_DE_SABLE_CHOICES = [
-        ('SABLE SANAGA', 'SABLE SANAGA'),
-        ('SABLE FIN', 'SABLE FIN'),
-    ]
-    type = models.CharField(max_length=20, choices=TYPE_DE_SABLE_CHOICES, verbose_name="Type de sable")
-    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Prix")
-    description = models.TextField()
-    def __str__(self):
-        return self.type
-
-class Service(models.Model):
-    sable = models.ForeignKey(Sable, on_delete=models.CASCADE, verbose_name='Sable')
-    name = models.CharField(max_length=100, verbose_name="nom")
-    description = models.TextField()
-    image = models.ImageField(upload_to='services/', null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
 class Realisation(models.Model):
-    service = models.ForeignKey(Service, on_delete=models.CASCADE, verbose_name='Service')
+    types=[
+    ('SABLE SANAGA','SABLE SANAGA'),
+    ('SABLE FIN','SABLE FIN')
+    ]
+    sable=models.CharField(choices=types,max_length=100)
     title = models.CharField(max_length=200)
     description = models.TextField()
     date = models.DateField(auto_now_add=True)
     image = models.ImageField(upload_to='realisations/', null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.sable
 
 class Message_User(models.Model):
     name = models.CharField(max_length = 30, default = 'inconnu')
