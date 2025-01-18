c81c21ddad7c80354dc2581adaf768c4bf091812
ramirokaffo <ramirokaffo>
2025-01-18 08:08:29 +0100

    Insertion du logo

--- site_web_laurant/settings.py
+++ site_web_laurant/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
 environ.Env.read_env(env_file=str(BASE_DIR/".env"))
 
+print(BASE_DIR)
+print(env)
 
-
-EMAIL_BACKEND = env("EMAIL_BACKEND")
 EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
 EMAIL_HOST = env("EMAIL_HOST")
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
@@ -32,6 +32,11 @@ EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = env("EMAIL_PORT")
 SERVER_EMAIL = env("SERVER_EMAIL")
 EMAIL_SUBJECT_PREFIX = env("EMAIL_SUBJECT_PREFIX")
+EMAIL_BACKEND = env("EMAIL_BACKEND")
+
+ADMINS = [
+('Laurant', 'qualisable.laurant@gmail.com'),
+]
 
 # ADMINS = env.list('ADMINS')
 
